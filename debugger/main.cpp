558642b3c0fe33e8139a83a0d726c97a2311c857
Felix Korch <korch.felix@gmail.com>
2020-02-18 21:25:46 +0100

    Build with framelesswindow

--- debugger/main.cpp
+++ debugger/main.cpp
@@ -1,4 +1,8 @@
 #include <QApplication>
+
+#include <darkstyle.h>
+#include <framelesswindow.h>
+
 #include "mainwindow.h"
 
 #ifdef _WIN32
@@ -8,9 +12,15 @@ int main(int argc, char *argv[])
 #endif
 {
     QApplication a(argc, argv);
+        a.setStyle(new DarkStyle);
+        // create frameless window (and set windowState or title)
+        FramelessWindow framelessWindow;
+        framelessWindow.setWindowTitle("LLVMES - Debugger");
+        framelessWindow.setFixedSize(800, 600);
 
-    MainWindow *mainWindow = new MainWindow;
-    mainWindow->show();
+        MainWindow *mainWindow = new MainWindow;
+        framelessWindow.setContent(mainWindow);
+        framelessWindow.show();
 
-    return a.exec();
+        return a.exec();
 }
