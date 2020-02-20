f616ec9d0772288317dd1dfa3b2581c413591dab
Markus Pettersson <markus.pettersson1998@gmail.com>
2020-02-20 15:56:01 +0100

    Add test program and data

---
+++ playground/ExecuteFile.cpp
@@ -0,0 +1,42 @@
+#include "llvmes/NES/CPU.h"
+#include "llvmes/NES/ProgramLoader.h"
+#include <string>
+#include <iostream>
+
+using namespace llvmes;
+
+std::vector<std::uint8_t> memory(0xFFFF);
+
+std::uint8_t readMemory(std::uint16_t adr) {
+    return memory[adr];
+}
+
+void writeMemory(std::uint16_t adr, std::uint8_t data) {
+    memory[adr] = data;
+}
+
+int main(int argc, char **argv) try {
+    ProgramLoader *programLoader = new ProgramLoader("xd.o");
+
+    auto program = programLoader->getProgram();
+
+    auto programTokens = programLoader->getProgramTokens();
+
+
+    // Load program into virtual computer memory
+    memory[0xFFFC] = 0x20;
+    memory[0xFFFD] = 0x40;
+    std::copy(program.begin(), program.end() - 1, &memory[0x4020]);
+
+    CPU cpu;
+    cpu.read = readMemory;
+    cpu.write = writeMemory;
+    cpu.reset();
+
+    cpu.run();
+
+    return 0;
+
+} catch (std::exception &e) {
+    std::cerr << e.what() << std::endl;
+}
