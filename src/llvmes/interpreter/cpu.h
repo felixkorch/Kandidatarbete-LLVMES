c65253b36c10c87f47ac1abe92117c395c10a24c
Felix Korch <korch.felix@gmail.com>
2020-02-25 12:56:39 +0100

    Add Hex-Int transformations in cpu.h

--- src/llvmes/interpreter/cpu.h
+++ src/llvmes/interpreter/cpu.h
@@ -144,4 +144,36 @@ class CPU {
     void IllegalOP();
 };
 
+template <typename T>
+inline std::string ToHexString(T i)
+{
+    std::stringstream stream;
+    stream << "$" << std::uppercase << std::setfill('0')
+           << std::setw(sizeof(T) * 2) << std::hex << (unsigned)i;
+    return stream.str();
+}
+
+template <>
+inline std::string ToHexString<bool>(bool i)
+{
+    std::stringstream stream;
+    stream << std::uppercase << std::setw(1) << std::hex << i;
+    return stream.str();
+}
+
+inline int HexStringToInt(const std::string& in)
+{
+    auto strip_zeroes = [](const std::string& s){
+        std::string temp = s;
+        while (temp.at(0) == '0')
+            temp = temp.substr(1);
+        return temp;
+    };
+
+    std::string out;
+    out = (in.at(0) == '$') ? strip_zeroes(in.substr(1))
+                            : strip_zeroes(in);
+    return std::stoi(out, 0, 16);
+}
+
 }  // namespace llvmes
