231d5e8d7ebdece1b6548d32dc2d5b7e40de3b8a
Matilda <falkenby@student.chalmers.se>
2020-02-26 12:14:19 +0100

    Fix the debugger on windows

--- debugger/main.cpp
+++ debugger/main.cpp
@@ -4,13 +4,9 @@
 #include <QApplication>
 
 #include "mainwindow.h"
+#include <windows.h>
 
-#ifdef _WIN32
-int WinMain(int argc,
-            char *argv[])  // Required to remove the console window on Windows
-#else
 int main(int argc, char *argv[])
-#endif
 {
     QApplication a(argc, argv);
     a.setStyle(new DarkStyle);
