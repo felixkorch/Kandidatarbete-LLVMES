7d372c831cf08f6eee52570662debb77d654c4a6
Felix Korch <korch.felix@gmail.com>
2020-03-23 03:21:34 +0100

    Implement a verison of make_unique

--- src/llvmes/common.h
+++ src/llvmes/common.h
@@ -5,19 +5,12 @@
 #include <sstream>
 #include <string>
 
-template <typename T>
-using Scope = std::unique_ptr<T>;
+namespace llvmes {
+
 template <typename T, typename... Args>
-constexpr Scope<T> CreateScope(Args&&... args)
+std::unique_ptr<T> make_unique(Args&&... args)
 {
     return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
-}
-template <typename T>
-using Ref = std::shared_ptr<T>;
-template <typename T, typename... Args>
-constexpr Ref<T> CreateRef(Args&&... args)
-{
-    return std::make_shared(std::forward<Args>(args)...);
 }
 
 template <typename T>
@@ -25,7 +18,7 @@ inline std::string ToHexString(T i)
 {
     std::stringstream stream;
     stream << "$" << std::uppercase << std::setfill('0')
-           << std::setw(sizeof(T) * 2) << std::hex << (unsigned)i;
+           << std::setw(sizeof(T) * 2) << std::hex << (unsigned int)i;
     return stream.str();
 }
 
@@ -40,7 +33,8 @@ inline std::string ToHexString<bool>(bool i)
 inline int HexStringToInt(const std::string& in)
 {
     auto strip_zeroes = [](std::string temp) {
-        while (temp.at(0) == '0') temp = temp.substr(1);
+        while (temp.at(0) == '0')
+            temp = temp.substr(1);
         return std::move(temp);
     };
 
@@ -48,3 +42,5 @@ inline int HexStringToInt(const std::string& in)
     out = (in.at(0) == '$') ? strip_zeroes(in.substr(1)) : strip_zeroes(in);
     return std::stoi(out, 0, 16);
 }
+
+}  // namespace llvmes
