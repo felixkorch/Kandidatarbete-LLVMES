fef1981ad1e509fab94d18d5cfc2eaf43050748f
Markus Pettersson <markus.pettersson1998@gmail.com>
2020-03-30 12:31:45 +0200

    add LSR Zero Page

--- src/llvmes/dynarec/codegen.cpp
+++ src/llvmes/dynarec/codegen.cpp
@@ -486,7 +486,19 @@ void Compiler::CodeGen(Instruction& i)
             DynamicTestN(value);
             break;
          }
-        case 0x46: {  // LSR Zeropage
+        case 0x46: { // LSR Zeropage
+            uint8_t addr = i.arg;
+            llvm::Value* ram_ptr = GetRAMPtr(addr);
+            llvm::Value* value = c->builder.CreateLoad(ram_ptr);
+            // Set carry flag to bit 0 of read value
+            llvm::Value* least_significant_bit = c->builder.CreateAnd(value, 1); //(value & 1);
+            c->builder.CreateStore(least_significant_bit, c->status_c);
+            // Do bit shift right and store the value
+            llvm::Value* right_shifted_value = c->builder.CreateLShr(value, 1); // value >> 1, logical shift right
+            c->builder.CreateStore(right_shifted_value, ram_ptr);
+            // Set Zero flag and Negative flag
+            DynamicTestZ(value);
+            DynamicTestN(value);
             break;
         }
         case 0x56: {  // LSR ZeropageX
