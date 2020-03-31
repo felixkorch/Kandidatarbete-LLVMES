b41f2ecf9a70f6edcc4d75f2f069502d2399219e
CLClund <carllun@net.chalmers.se>
2020-03-31 13:31:55 +0200

    created cmp absolute Y op$D9

--- src/llvmes/dynarec/codegen.cpp
+++ src/llvmes/dynarec/codegen.cpp
@@ -177,6 +177,22 @@ namespace llvmes {
                 break;
             }
             case 0xD9: { // CMP AbsoluteY
+                // in data
+                llvm::Value* ram_ptr = GetRAMPtr(i.arg);
+                // get reg_y
+                llvm::Value* reg_y = c->builder.CreateLoad(c->reg_y);
+                // add reg_y and ram_ptr
+                llvm::Value* memPoiter = c->builder.CreateAdd(ram_ptr, reg_y);
+                // get date from pointer
+                llvm::Value* load_ram = c->builder.CreateLoad(memPoiter);
+                // get reg_a
+                llvm::Value* reg_a = c->builder.CreateLoad(c->reg_a);
+                // compare
+                llvm::Value* result = c->builder.CreateSub(reg_a, load_ram);
+                // flag test
+                DynamicTestZ(result);
+                DynamicTestN(result);
+                DynamicTestCCmp(result);
                 break;
             }
             case 0xC1: { // CMP IndirectX
