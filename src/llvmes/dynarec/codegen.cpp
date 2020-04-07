0314df9ed2b2f0596a4486d3b969a581b2e38565
Matilda <falkenby@student.chalmers.se>
2020-04-07 13:44:18 +0200

    fix LDY absolute X

--- src/llvmes/dynarec/codegen.cpp
+++ src/llvmes/dynarec/codegen.cpp
@@ -442,11 +442,8 @@ void Compiler::CodeGen(Instruction& i)
             // Loads the X register into a placeholder
             llvm::Value* load_x = c->builder.CreateLoad(c->reg_x);
             // Adds the X register to the RAM pointer
-            llvm::Value* index_16 = c->builder.CreateAdd(ram_ptr, load_x);
-            // AND with 0xFFF to make sure that the index is 3 byte
-            llvm::Value* zero_page_index =
-                c->builder.CreateAnd(index_16, 0xFFF);
-            llvm::Value* value = c->builder.CreateLoad(zero_page_index);
+            llvm::Value* target_addr = c->builder.CreateAdd(ram_ptr, load_x);
+            c->builder.CreateCall(c->read_fn, {target_addr, load_x});
             break;
         }
         case 0x4A: {  // ACC Accumulator
