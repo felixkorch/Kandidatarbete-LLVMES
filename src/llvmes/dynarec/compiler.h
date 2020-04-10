3a8003d88bbe7f41eb8b035beff2c15c039bedef
CLClund <carllun@net.chalmers.se>
2020-04-10 16:06:46 +0200

    Fixed so that C flag sets right

--- src/llvmes/dynarec/compiler.h
+++ src/llvmes/dynarec/compiler.h
@@ -129,7 +129,7 @@ class Compiler {
     void DynamicTestCCmp(llvm::Value* v)
     {
         llvm::Constant* c_0x0100 = llvm::ConstantInt::get(int16, 0x0100);
-        llvm::Value* lesThen = c->builder.CreateICmpULT(v, c_0x0100);
+        llvm::Value* lesThen = c->builder.CreateICmpUGT(v, c_0x0100);
         c->builder.CreateStore(lesThen, c->status_c);
     }
     // Calculates the ram-address as a constant-expr
