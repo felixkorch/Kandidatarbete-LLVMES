4b63e3d21a818b6480aa3261856f1df751a8caf8
CLClund <carllun@net.chalmers.se>
2020-04-15 17:50:11 +0200

    Add ROR Absolute

--- src/llvmes/dynarec/codegen.cpp
+++ src/llvmes/dynarec/codegen.cpp
@@ -722,6 +722,29 @@ void Compiler::CodeGen(Instruction& i)
             break;
         }
         case 0x6E: {  // ROR Absolute
+            llvm::Value* addr = AddressModeAbsolute(i.arg);
+            llvm::Value* operand = c->builder.CreateCall(c->read_fn, addr);
+            // Get status_c
+            llvm::Value* carry_in = c->builder.CreateLoad(c->status_c);
+            // Get carry_out
+            llvm::Value* carry_out =
+                c->builder.CreateAnd(operand, GetConstant8(0x01));
+            // Shift reg_a
+            llvm::Value* operand_Shr = c->builder.CreateLShr(operand, 1);
+            // Add carry_in
+            llvm::Value* carry_in_8 = c->builder.CreateZExt(carry_in, int8);
+            llvm::Value* carry_in_shl = c->builder.CreateShl(carry_in_8, 7);
+            llvm::Value* result =
+                c->builder.CreateOr(operand_Shr, carry_in_shl);
+            // Stor reg_a
+            c->builder.CreateStore(result, c->reg_a);
+            // Set status_c
+            llvm::Value* carry_out_1 =
+                c->builder.CreateICmpEQ(carry_out, GetConstant8(0x01));
+            c->builder.CreateStore(carry_out_1, c->status_c);
+            // Flag test
+            DynamicTestZ(result);
+            DynamicTestN(result);
             break;
         }
         case 0x7E: {  // ROR AbsoluteX
