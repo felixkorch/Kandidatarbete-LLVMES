c285c1e8024b4ea09c4936834e47a5b549c5a38b
Matilda <falkenby@student.chalmers.se>
2020-04-16 21:48:56 +0200

    Test INC zeropage X
    test not working yet

--- playground/compile_test.cpp
+++ playground/compile_test.cpp
@@ -87,12 +87,14 @@ std::vector<uint8_t> testBranches{
     PRINT_A,  // FF
 };
 
-std::vector<uint8_t> testINC_zeropage{
+std::vector<uint8_t> testINC_zeropageX{
     0xA9, 0x0A,  // LDA; # 0x0A
     0x8D, 0x09, 0x20,  // Print A - should print 0x0A = 10
-    0x8D, 0x20, 0x00,  // reg_a (=0x0A) to 0x0020 in mem
+    0x8D, 0x21, 0x00,  // reg_a (=0x0A) to 0x0020 in mem
     LDX_IMM(0x01),
     0xF6, 0x20,  // inc zeropage mem 0x0020 + x
+    0xB5, 0x20,
+    0x8D, 0x09, 0x20,  // Print A - should print 0x0A = 10
     0x8D, 0x0C, 0x20,  // Print status N - should print 00
     0x8D, 0x0E, 0x20,  // Print status Z - should print 01
 };
@@ -566,7 +568,8 @@ using namespace llvmes;
 
 int main()
 {
-    auto d = llvmes::make_unique<Disassembler>(std::move(cmp_AbsoluteY), 0x8000);
+
+    auto d = llvmes::make_unique<Disassembler>(std::move(testINC_zeropageX));
 
     AST ast;
     std::vector<uint8_t> ram;
