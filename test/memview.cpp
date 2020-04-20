b22063c3d30985feeb3104777c8785fe5bb5a46d
Felix Korch <korch.felix@gmail.com>
2020-04-20 20:31:39 +0200

    Add program that can print parts of a binary and compare binaries

--- test/memview.cpp
+++ test/memview.cpp
@@ -31,14 +31,14 @@ using namespace llvmes;
 std::pair<int, int> ParseRange(const std::string& first,
                                const std::string& second)
 {
-    int first_int = HexStringToInt(first);
-    int second_int = HexStringToInt(second);
+    int first_int = std::stoi(first, 0, 16);
+    int second_int = std::stoi(second, 0, 16);
     return std::make_pair(first_int, second_int);
 }
 
 int main(int argc, char** argv)
 try {
-    cxxopts::Options options("Mem Explorer",
+    cxxopts::Options options("MemView",
                              "Print ranges of memory | compare bin files");
 
     options.add_options()("f,positional", "File",
@@ -46,11 +46,14 @@ try {
         "c,compare", "Compare two bin files",
         cxxopts::value<std::vector<std::string>>())(
         "r,range", "Range of memory",
-        cxxopts::value<std::vector<std::string>>())(
-        "p,print", "Print?", cxxopts::value<bool>())("h,help", "Print usage");
+        cxxopts::value<std::vector<std::string>>())("p,print", "Prints memory",
+                                                    cxxopts::value<bool>())(
+        "h,help",
+        "To print a range of memory: memview -p -r 0x0000,0x2000 "
+        "file.mem\nNotice no space between arguments and '0x' notation");
 
-    auto result = options.parse(argc, argv);
     options.parse_positional({"positional"});
+    auto result = options.parse(argc, argv);
 
     if (result.count("help")) {
         std::cout << options.help() << std::endl;
@@ -64,27 +67,34 @@ try {
         bool eq = CompareBins(input[0], input[1]);
         std::cout << (eq ? "Equal" : "Not equal") << std::endl;
     }
-    else if (result.count("print")) {
-        int range_count = result.count("range");
-        if (range_count && range_count != 2)
+    if (result.count("print")) {
+        bool use_range = false;
+        std::vector<std::string> range;
+        if (result.count("range")) {
+            use_range = true;
+            range = result["range"].as<std::vector<std::string>>();
+        }
+        if (use_range && range.size() != 2)
             throw std::runtime_error("Range takes two values!");
 
         uint16_t start, stop;
         start = 0x0000;
         stop = 0xFFFF;
-        if (range_count) {
-            auto range = result["range"].as<std::vector<std::string>>();
+        if (use_range) {
             auto pair = ParseRange(range[0], range[1]);
             start = pair.first;
             stop = pair.second;
         }
-
         auto in_file = result["positional"].as<std::string>();
         auto vec = ReadFile(in_file);
+
+        if (stop > vec.size())
+            stop = vec.size();
 
         for (int i = start; i < stop; i++) {
             std::cout << ToHexString(vec.at(i)) << " ";
         }
+        std::cout << std::endl;
     }
 }
 catch (std::exception& e) {
