3e8c29671310efb763feb1231380288a781efb76
Felix Korch <korch.felix@gmail.com>
2020-04-30 23:08:09 +0200

    Add simple test for gui

---
+++ playground/gui_test.cpp
@@ -0,0 +1,47 @@
+#include <imgui.h>
+#include <llvmes-gui/application.h>
+#include <llvmes-gui/log.h>
+
+#include <iostream>
+
+using namespace llvmes;
+
+class Debugger : public gui::Application {
+    bool p_open = true;
+
+   public:
+    Debugger() : Application(1200, 800, "Test UI") {}
+
+    void OnImGui() override
+    {
+        // ImGui::ShowDemoWindow();
+    }
+
+    void OnEvent(gui::Event& e) override
+    {
+        if (e.GetEventType() == gui::EventType::KeyPressEvent) {
+            auto& ev = (gui::KeyPressEvent&)e;
+            LLVMES_INFO("Key down: {}", ev.GetKeyCode());
+
+            if (ev.GetKeyCode() == LLVMES_KEY_G) {
+                LLVMES_TRACE("G key down!");
+            }
+        }
+        else if (e.GetEventType() == gui::EventType::MouseMoveEvent) {
+            auto& ev = (gui::MouseMoveEvent&)e;
+            auto pair = ev.GetPosition();
+            LLVMES_TRACE("Mouse position: {} {}", pair.first, pair.second);
+        }
+    }
+
+    void OnUpdate() override {}
+};
+
+int main()
+try {
+    Debugger ui;
+    ui.Run();
+}
+catch (std::exception& e) {
+    std::cerr << e.what() << std::endl;
+}
